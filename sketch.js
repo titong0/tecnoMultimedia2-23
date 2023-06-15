a639ab3cfe88f68d641cf48c3a0fa0fb1f997d86
JuaniErrecarte <103791244+JuaniErrecarte@users.noreply.github.com>
2023-06-14 23:40:38 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -56,7 +56,7 @@ function setup() {
 }
 
 function draw() {
-  background(2119, 125, 121);
+  background(219, 125, 121);
   if (label == "Class 2") {
     const indexPincelada = parseInt(random(5, 6));
     const pincelada = pinceladas[indexPincelada];
